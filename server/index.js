8b01a76a3fc10624b6236772a1e025ded89c5fe7
Zhengjie Zhou (Jay) <zhengjiezhou@dal.ca>
2022-06-28 22:49:11 -0300

    Backend running

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 
 const app = express();
 
-app.use(bodyParser.json({limit: "30mb", extended: true}))
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://dev:n4qCdwPiG4XC1uJg@cluster0.ackhx.mongodb.net/?retryWrites=true&w=majority'
+// Avoid committing with credentials. Always revert to <password>.
+const CONNECTION_URL =
+  "mongodb+srv://dev:<password>@cluster0.ackhx.mongodb.net/?retryWrites=true&w=majority";
+// Locally run on 5051.
+const PORT = process.env.PORT || 5051;
 
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log("Server running on PORT " + PORT))
+  )
+  .catch((error) => console.log(error.message));
 
 // https://
