e8d940f864cce79b4bfbf33070aea794eb1a575b
Zhengjie Zhou (Jay) <zhengjiezhou@dal.ca>
2022-08-12 08:06:01 -0300

    The floe is flowing

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -6,3 +6,13 @@ export const getPosts = () => async (dispatch) => {
     dispatch({ type: "FETCH_ALL", payload: data });
   } catch (error) {}
 };
+
+export const createPost = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(post);
+
+    dispatch({ type: "CREATE", payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,3 +3,4 @@ import axios from "axios";
 const url = "http://localhost:5051/posts";
 
 export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost);
