31a286fec04654dcb2e7558e6ac5faa183ed75f0
Zhengjie Zhou (Jay) <zhengjiezhou@dal.ca>
2022-08-13 19:48:52 -0300

    Complete

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -24,4 +26,18 @@ export const createPost = async (req, res) => {
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(402).send("No post with that id");
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  });
+
+  res.json(updatedPost);
 };
