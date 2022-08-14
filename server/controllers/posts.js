e52d869b842be355e95e1b00c928ee297db45f9b
Zhengjie Zhou (Jay) <zhengjiezhou@dal.ca>
2022-08-13 21:23:37 -0300

    Complete

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -33,11 +33,22 @@ export const updatePost = async (req, res) => {
   const post = req.body;
 
   if (!mongoose.Types.ObjectId.isValid(_id))
-    return res.status(402).send("No post with that id");
+    return res.status(404).send("No post with that id");
 
   const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
     new: true,
   });
 
   res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+  const { id: _id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post with that id");
+
+  await PostMessage.findByIdAndRemove(_id);
+
+  res.json({ message: "Post deleted successfully" });
 };
